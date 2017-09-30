03e6acdce2854adc4539cd9489a7236a3ab76b92
Charlie Boucher <charlie.boucher@edhec.com>
2017-09-30 16:43:49 +0200

    exercise 1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # get 'restaurants/:id/review', to: 'restaurants#new_review'
+
+  resources :restaurants, only: [:create, :index, :show, :new] do
+      resources :reviews, only: [:new, :create]
+      # get 'review', to: 'restaurants#new_review'
+      # post  to: 'restaurants#create_review'
+  end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+15.times do
+  restaurant = Restaurant.new(
+    name: Faker::Food.dish,
+    address: Faker::Address.street_address,
+    category: %w(french japanese chinese belgian italian).sample
+    )
+  restaurant.save
+  8.times do
+    review = Review.new(
+      content: Faker::HowIMetYourMother.quote,
+      rating: (2..5).to_a.sample,
+      restaurant: restaurant
+    )
+    review.save
+  end
+end
